402f077402a5be2f510d63537cd11ed0d09dd7e1
zenatureza <arthur.pinheiro357@gmail.com>
2021-02-21 14:55:25 -0300

    fixes database name env variable

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ const baseDir = process.env.NODE_ENV !== 'development' ? 'dist' : 'src';
 
 const config: ConnectionOptions = {
   type: 'sqlite',
-  database: process.env.POSTGRES_DATABASE ?? 'omnichat.sqlite',
+  database: process.env.DATABASE_NAME ?? 'omnichat.sqlite',
   entities: [`./${baseDir}/modules/**/infra/typeorm/entities/*.entity.{ts,js}`],
   migrations: [`./${baseDir}/database/migrations/*.{ts,js}`],
   cli: {
